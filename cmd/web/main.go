5987b02f630fb445c0d1326be25e5708c49bd452
Edwin Ade <edwinmbonyjr@gmail.com>
2025-06-21 01:48:23 +0100

    feat: refactoring routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -17,32 +17,12 @@ func main() {
 	debug := flag.Bool("debug", false, "Debug mode on/off")
 	flag.Parse()
 
-	var logLevel slog.Level
-	if *debug {
-		logLevel = slog.LevelDebug
-	} else {
-		logLevel = slog.LevelInfo
-	}
-	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		Level: logLevel,
-	}))
-
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
 	app := &Application{
-		logger: logger,
+		logger: NewLogger(debug),
 	}
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippets/{id}", app.getSnippet)
-	mux.HandleFunc("GET snippets/create", app.getSnippetForm)
-	mux.HandleFunc("POST snippets/create", app.createSnippet)
 
-	logger.Info("Starting a new server", slog.Any("port", *port))
-	err := http.ListenAndServe(fmt.Sprintf(":%s", *port), mux)
-	logger.Error(err.Error())
+	app.logger.Info("Starting a new server", slog.Any("port", *port))
+	err := http.ListenAndServe(fmt.Sprintf(":%s", *port), app.routes())
+	app.logger.Error(err.Error())
 	os.Exit(1)
 }
---
+++ cmd/web/routes.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+func (app *Application) routes() http.Handler {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippets/{id}", app.getSnippet)
+	mux.HandleFunc("GET snippets/create", app.getSnippetForm)
+	mux.HandleFunc("POST snippets/create", app.createSnippet)
+	return mux
+}
