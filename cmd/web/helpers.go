56b11498f08ff8c82e3c58796287d0b032c5b0e7
Edwin Ade <edwinmbonyjr@gmail.com>
2025-06-21 01:47:58 +0100

    feat: add stack traces

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"log/slog"
 	"net/http"
+	"os"
+	"runtime/debug"
 )
 
+func NewLogger(debugFlag *bool) *slog.Logger {
+	var logLevel slog.Level
+	if *debugFlag {
+		logLevel = slog.LevelDebug
+	} else {
+		logLevel = slog.LevelInfo
+	}
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: logLevel,
+	}))
+	return logger
+}
+
 func (app *Application) serverError(w http.ResponseWriter, r *http.Request, err error) {
-	method, uri := r.Method, r.URL.RequestURI()
-	app.logger.Error(err.Error(), "method", method, "uri", uri)
+	method, uri, trace := r.Method, r.URL.RequestURI(), string(debug.Stack())
+	app.logger.Error(err.Error(), "method", method, "uri", uri, "trace", trace)
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
