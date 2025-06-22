dc02c803d3ae8b42c7e62356233347ed42336b66
Edwin Ade <edwinmbonyjr@gmail.com>
2025-06-22 23:06:15 +0100

    feat: update js to use let/const syntax instead of var

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,6 +1,6 @@
-var navLinks = document.querySelectorAll("nav a");
-for (var i = 0; i < navLinks.length; i++) {
-	var link = navLinks[i]
+const navLinks = document.querySelectorAll("nav a");
+for (let i = 0; i < navLinks.length; i++) {
+	let link = navLinks[i]
 	if (link.getAttribute('href') == window.location.pathname) {
 		link.classList.add("live");
 		break;
