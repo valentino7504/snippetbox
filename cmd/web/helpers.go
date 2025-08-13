55a6d5598c111b9815487f59afbc2be82a7b052a
Edwin Ade <edwinmbonyjr@gmail.com>
2025-08-13 01:09:16 +0100

    feat(templates): add rendering for template cache

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,13 +1,50 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"runtime/debug"
+	"time"
 )
 
-func NewLogger(debugFlag *bool) *slog.Logger {
+func (app *Application) serverError(w http.ResponseWriter, r *http.Request, err error) {
+	method, uri, trace := r.Method, r.URL.RequestURI(), string(debug.Stack())
+	app.logger.Error(err.Error(), "method", method, "uri", uri, "trace", trace)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (app *Application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+func (app *Application) render(w http.ResponseWriter, r *http.Request, status int, page string, data templateData) {
+	ts, ok := app.templateCache[page]
+	if !ok {
+		err := fmt.Errorf("the template %s does not exist", page)
+		app.serverError(w, r, err)
+		return
+	}
+
+	buf := new(bytes.Buffer)
+
+	err := ts.ExecuteTemplate(buf, "base", data)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	w.WriteHeader(status)
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+}
+
+func newLogger(debugFlag *bool) *slog.Logger {
 	var logLevel slog.Level
 	if *debugFlag {
 		logLevel = slog.LevelDebug
@@ -20,12 +57,6 @@ func NewLogger(debugFlag *bool) *slog.Logger {
 	return logger
 }
 
-func (app *Application) serverError(w http.ResponseWriter, r *http.Request, err error) {
-	method, uri, trace := r.Method, r.URL.RequestURI(), string(debug.Stack())
-	app.logger.Error(err.Error(), "method", method, "uri", uri, "trace", trace)
-	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-}
-
-func (app *Application) clientError(w http.ResponseWriter, status int) {
-	http.Error(w, http.StatusText(status), status)
+func (app *Application) newTemplateData(r *http.Request) templateData {
+	return templateData{CurrentYear: time.Now().Year()}
 }
