ce23f696a1c4fcd40e01877167be6197fe03cce8
Edwin Ade <edwinmbonyjr@gmail.com>
2025-08-13 01:07:35 +0100

    feat(templates): add template caching

---
+++ cmd/web/templates.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"html/template"
+	"path/filepath"
+	"time"
+
+	_ "github.com/valentino7504/snippetbox/internal/models"
+)
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 3:04pm")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		// add partials to this template set
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+		cache[name] = ts
+	}
+	return cache, nil
+}
